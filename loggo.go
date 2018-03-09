ec419af314594daca2ebc61dd3efae6900cfebf6
imKiran <kiransuvarna19@gmail.com>
2018-03-09 11:39:13 +0530

    The simple logger

---
+++ loggo.go
@@ -0,0 +1,37 @@
+package loggo
+
+import (
+	"io"
+	"log"
+)
+
+var (
+	TRACE   *log.Logger
+	INFO    *log.Logger
+	WARNING *log.Logger
+	ERROR   *log.Logger
+)
+
+//InitLog : This is the Logger Utility
+func InitLog(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	TRACE = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	INFO = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	WARNING = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	ERROR = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
